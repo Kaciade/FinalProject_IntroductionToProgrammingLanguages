ca28feefe359a5bad7e9a7a89b9cff0362beb236
Kaciade <valerypolyakov60@gmail.com>
2023-02-25 12:42:43 +0400

    Задание 1 выполнено

---
+++ Tasks/Task_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы.
+
+Console.Write("\nВведите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"\nПоследовательность натуральных чисел от {m} до {n}:");
+while (m <= n)
+{
+    Console.Write($"{m}\t");
+    m++;
+}
+Console.WriteLine("\n");
