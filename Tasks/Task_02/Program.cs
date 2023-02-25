0ad0fca8339f50cfb6b7a7da7aea6cdd32188041
Kaciade <valerypolyakov60@gmail.com>
2023-02-25 13:10:37 +0400

    Задание 2 выполнено

---
+++ Tasks/Task_02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.Write("\nВведите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int result = 0;
+
+for (int i = m; i <= n; i++)
+{
+    result += i;
+}
+Console.WriteLine($"\nСумма элементов от {m} до {n} = {result}\n");
