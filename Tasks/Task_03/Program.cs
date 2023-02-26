1bf1aed1d49834230dcbc1af047851fedb98edfc
Kaciade <valerypolyakov60@gmail.com>
2023-02-26 20:03:23 +0400

    Задание 3 выполнено

---
+++ Tasks/Task_03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму цифр числа
+
+Console.Write("\nВведите число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+int num2 = num1;
+int sum = 0;
+
+while (num2 > 0)
+{
+    int digit = num2 % 10;
+    sum = sum + digit;
+    num2 = num2 / 10;
+}
+
+Console.WriteLine($"\nСумма цифр числа {num1} равна {sum}\n");
