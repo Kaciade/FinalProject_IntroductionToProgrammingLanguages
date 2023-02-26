68fc9707c376ca4cf3ed7651b326ee16556cca9f
Kaciade <valerypolyakov60@gmail.com>
2023-02-26 20:12:43 +0400

    Задание 4 выполнено

---
+++ Tasks/Task_04/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу вычисления функции Аккермана
+
+Console.Write("\nВведите число M: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+AkkermanFunction(m,n);
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write($"\nA({m},{n}) = {Akkerman(m, n)}\n");
+    Console.WriteLine();
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
