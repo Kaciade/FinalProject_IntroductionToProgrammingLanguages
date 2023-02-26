d3b2b6fff3da8ee90e3d3e98e8c569b5b247890a
Kaciade <valerypolyakov60@gmail.com>
2023-02-26 20:20:50 +0400

    Задание 5 выполнено

---
+++ Tasks/Task_05/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+Console.Write("\nВведите число A: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+int result = a;
+
+for (int i = 1; i < b; i++)
+{
+    result = result * a;
+}
+
+Console.WriteLine($"\nA^B = {result}\n");
