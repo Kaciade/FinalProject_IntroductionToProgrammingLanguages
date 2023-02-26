db688d5aa02ec95b3e2104899ed1fe4947d2cbda
Kaciade <valerypolyakov60@gmail.com>
2023-02-26 19:53:52 +0400

    Задание 1 переделано, чтобы числа выводились в терминал более удобным образом

--- Tasks/Task_01/Program.cs
+++ Tasks/Task_01/Program.cs
@@ -4,11 +4,40 @@ Console.Write("\nВведите число M: ");
 int m = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите число N: ");
 int n = int.Parse(Console.ReadLine() ?? "0");
+int count = 0;
+int m1 = m;
+
+while (m1 <= n)
+{
+    count++;
+    m1++;
+}
+
+if (count % 10 != 0)
+{
+    while (count % 10 > 0) count++;
+}
+
+int m2 = m;
+int [,] matrix = new int [count/10, 10];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (m2 <= n) matrix[i, j] = m2;
+        else matrix[i, j] = 0;
+        m2++;
+    }
+}
 
 Console.WriteLine($"\nПоследовательность натуральных чисел от {m} до {n}:");
-while (m <= n)
+
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    Console.Write($"{m}\t");
-    m++;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i, j] == 0 && i != 0 && j != 0) break;
+        Console.Write($"{matrix[i, j]}\t");
+    }
+    Console.WriteLine();
 }
-Console.WriteLine("\n");
